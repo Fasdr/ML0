7e53eb249695a5d4837ac643574e62060ea74dba
saa19 <saaa1997@gmail.com>
2019-07-01 03:20:10 +0700

    1

--- project1(sentiment_analysis)/main.py
+++ project1(sentiment_analysis)/main.py
@@ -105,15 +105,15 @@ data = (train_bow_features, train_labels, val_bow_features, val_labels)
 # test_bow_features and test_labels.
 #-------------------------------------------------------------------------------
 
-# (fr, sr) = p1.classifier_accuracy(p1.pegasos, train_bow_features, test_bow_features, train_labels, test_labels, T=25, L=0.01)
-# print(sr)
+(fr, sr) = p1.classifier_accuracy(p1.pegasos, train_bow_features, test_bow_features, train_labels, test_labels, T=25, L=0.01)
+print(sr)
 
 #-------------------------------------------------------------------------------
 # Assign to best_theta, the weights (and not the bias!) learned by your most
 # accurate algorithm with the optimal choice of hyperparameters.
 #-------------------------------------------------------------------------------
 
-# best_theta = None # Your code here
+# (best_theta,ss) = p1.pegasos(train_bow_features, train_labels, 25, 0.01)  # Your code here
 # wordlist   = [word for (idx, word) in sorted(zip(dictionary.values(), dictionary.keys()))]
 # sorted_word_features = utils.most_explanatory_word(best_theta, wordlist)
 # print("Most Explanatory Word Features")
