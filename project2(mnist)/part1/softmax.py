258956c79eb74fe5576f191397951a21021ce66e
saa19 <saaa1997@gmail.com>
2019-07-12 21:06:04 +0700

    1

--- project2(mnist)/part1/softmax.py
+++ project2(mnist)/part1/softmax.py
@@ -115,8 +115,7 @@ def update_y(train_y, test_y):
         test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                     for each datapoint in the test set
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+    return (train_y % 3, test_y % 3)
 #pragma: coderesponse end
 
 #pragma: coderesponse template
@@ -135,8 +134,8 @@ def compute_test_error_mod3(X, Y, theta, temp_parameter):
     Returns:
         test_error - the error rate of the classifier (scalar)
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+    T = get_classification(X, theta, temp_parameter)
+    return 1 - np.mean((Y % 3) == (T % 3))
 #pragma: coderesponse end
 
 def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
