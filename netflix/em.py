fed602671fdc8a776d2c2c9e64e724866d7632cb
saa19 <saaa1997@gmail.com>
2019-08-19 22:10:32 +0700

    1

--- netflix/em.py
+++ netflix/em.py
@@ -18,8 +18,48 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         float: log-likelihood of the assignment
 
     """
-    raise NotImplementedError
 
+    mu = mixture.mu
+    var = mixture.var
+    p = mixture.p
+
+    k, = var.shape
+    n, d = X.shape
+
+    mask = np.where(X != 0, 1, 0)
+    dim_X = mask.sum(axis=1)
+    dim_X = dim_X[:, None]
+    dim_X = np.tile(dim_X, (1, k))
+
+    new_X = X[:, :, None]
+    new_X = np.tile(new_X, (1, 1, k))
+    new_X = np.swapaxes(new_X, 1, 2)
+
+    new_var = var[:, None]
+    new_var = np.tile(new_var, (1, n))
+    new_var = np.swapaxes(new_var, 0, 1)
+
+    new_mu = mu[:, :, None]
+    new_mu = np.tile(new_mu, (1, 1, n))
+    new_mu = np.swapaxes(new_mu, 0, 2)
+    new_mu = np.swapaxes(new_mu, 1, 2)
+
+    args = np.where(new_X == 0, 0, new_X - new_mu)
+    norms = (args ** 2).sum(axis=2)
+    N = ((2 * np.pi * new_var) ** (-dim_X / 2)) * np.exp(-((2 * new_var) ** (-1)) * norms)
+
+    new_p = p[:, None]
+    new_p = np.tile(new_p, (1, n))
+    new_p = np.swapaxes(new_p, 0, 1)
+    new_log_p = np.log(1e-16 + new_p)
+    f = new_log_p + np.log(N)
+    loss = (np.exp(f).sum(axis=1))
+    out_loss = np.log(loss).sum()
+    new_f = np.log(np.tile(loss[:, None], (1, k)))
+    final_f = f - new_f
+    out_p = np.exp(final_f)
+
+    return out_p, out_loss
 
 
 def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
@@ -37,7 +77,8 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    _, k = post.shape
+    n, d = X.shape
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
--- netflix/main.py
+++ netflix/main.py
@@ -10,17 +10,17 @@ X = np.loadtxt("toy_data.txt")
 # TODO: Your code here
 #
 # for k in range(1, 5, 1):
-#     for i in range(5):
+#     for i in range(1):
 #         gaussian, post = common.init(X, k, seed=i)
 #         gaussian, post, new_ll = kmeans.run(X, gaussian, post)
 #         common.plot(X, gaussian, post, "K-means: number of classes{}, random seed {}".format(k, i))
 #
 # for k in range(1, 5, 1):
-#     for i in range(5):
+#     for i in range(1):
 #         gaussian, post = common.init(X, k, seed=i)
 #         gaussian, post, new_ll = naive_em.run(X, gaussian, post)
 #         common.plot(X, gaussian, post, "EM: number of classes{}, random seed {}".format(k, i))
-#
+
 
 
 
@@ -31,3 +31,4 @@ X = np.loadtxt("toy_data.txt")
 #         print("BIC = {} for K = {} and seed = {}".format(common.bic(X, gaussian, new_ll), k, i))
 #
 #
+
